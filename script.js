3723595263b17d436da1b7a19d4530c9a48d866a
Molid <delmolid@hotmail.fr>
2025-01-22 17:39:54 +0100

    solution etape 1

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-index.html
-script.js
+import { dossierPrincipal } from "./data.js"; // Import du fichier data.js
+
+// Fonction permettant d'afficher le nom du dossier principal
+function afficherDossier(dossierPrincipal) {
+  return dossierPrincipal.nom;
+}
+
+console.log(afficherDossier(dossierPrincipal));
+
+// Fonction récursive permettant d'afficher le contenu du dossier principale Ada
+function afficherDossierIteratif(dossierPrincipal) {
+ let contenu = dossierPrincipal.contenu;
+ console.log(contenu);
+ let collectif = [];
+ for (let tableau of contenu) {
+  console.log(tableau.nom) 
+    
+}
+}
+afficherDossierIteratif(dossierPrincipal)
+
+function afficherDossierRecursif(dossierPrincipal) {
+  if (dossierPrincipal.contenu.length === 0) {
+    console.log(dossierPrincipal.nom); 
+    afficherDossierRecursif(dossierPrincipal.contenu);
+  } else {
+      for (let i = 0; i < dossierPrincipal.contenu.length; i++) {
+        console.log(dossierPrincipal.contenu[i].nom);
+   }
+    }
+
+
+  }
+  
+afficherDossierRecursif(dossierPrincipal);
